e1dc0af07bd73c2b2ec67de1ad577ac46fe3feea
Angytap82 <angytap82@gmail.com>
2026-04-15 17:25:33 -0300

    Trabajo_ Integrador P1

---
+++ main.py
@@ -0,0 +1,61 @@
+import csv
+
+def cargar_datos(nombre_archivo):
+    lista_paises = []
+    try:
+        with open(nombre_archivo, mode='r', encoding='utf-8') as archivo:
+            lector = csv.DictReader(archivo)
+            for fila in lector:
+                # Convertimos los números de texto a enteros para poder calcular
+                fila['poblacion'] = int(fila['poblacion'])
+                fila['superficie'] = int(fila['superficie'])
+                lista_paises.append(fila)
+        return lista_paises
+    except FileNotFoundError:
+        print("\n[Error] No se encontró el archivo 'paises.csv'.")
+        return []
+
+def buscar_pais(lista, nombre_buscar):
+    for pais in lista:
+        if pais['nombre'].lower() == nombre_buscar.lower():
+            return pais
+    return None
+
+def main():
+    paises = []
+    
+    while True:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Cargar países desde CSV")
+        print("2. Buscar un país por nombre")
+        print("3. Salir")
+        
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            paises = cargar_datos("paises.csv")
+            if paises:
+                print(f"\n¡Éxito! Se cargaron {len(paises)} países.")
+        
+        elif opcion == "2":
+            if not paises:
+                print("\n[!] Primero debes cargar los datos (Opción 1).")
+            else:
+                nombre = input("Nombre del país a buscar: ")
+                resultado = buscar_pais(paises, nombre)
+                if resultado:
+                    print(f"\nDatos de {resultado['nombre']}:")
+                    print(f"- Población: {resultado['poblacion']}")
+                    print(f"- Superficie: {resultado['superficie']} km²")
+                    print(f"- Continente: {resultado['continente']}")
+                else:
+                    print("\nPaís no encontrado.")
+
+        elif opcion == "3":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    main()
